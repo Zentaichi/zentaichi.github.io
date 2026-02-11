bd1ccca17502ee284b350a8f845bac3b98032d51
Ernest Endrino <ernest.endrino@gmail.com>
2026-02-11 13:42:14 +0800

    feat(analytics): integrate umami for page visits

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,6 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: 'Zentaichi Portfolio',
   description: 'The portfolio of Ernest Endrino, full stack developer',
-  generator: 'v0.app',
   icons: {
     icon: [
       {
@@ -38,6 +37,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="744bdfc7-0f49-4ef3-9032-27fd57ab37ab"></script>
+      </head>
       <body className={`font-sans antialiased`}>
         <CustomCursor />
         <AnimatedBackground />
